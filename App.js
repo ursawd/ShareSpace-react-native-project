7ac5e73dd7e153a323d41a3d74cda46af362912b
Phil Bailey <philbailey@msn.com>
2022-01-19 14:06:07 -0500

    refactor init project to individual files

--- App.js
+++ App.js
@@ -4,22 +4,8 @@ import * as React from "react";
 import { Button, View } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={() => navigation.navigate("Notifications")} title="Go to notifications" />
-    </View>
-  );
-}
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
+import HomeView from "./views/HomeView";
+import MembershipView from "./views/MembershipView";
 
 const Drawer = createDrawerNavigator();
 
@@ -27,8 +13,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        <Drawer.Screen name="Home" component={HomeView} />
+        <Drawer.Screen name="Membership" component={MembershipView} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
