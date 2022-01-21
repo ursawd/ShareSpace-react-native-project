55e17f3927c270674851045d1dd5b99ee6898a4d
Phil Bailey <philbailey@msn.com>
2022-01-20 22:27:54 -0500

    add row 4

---
+++ views/Home/Row4.js
@@ -0,0 +1,30 @@
+import { View, Text, ImageBackground } from "react-native";
+import React from "react";
+import gStyles from "../../gStyles";
+import { Button } from "react-native-elements";
+
+const Row4 = () => {
+  return (
+    <ImageBackground source={require("../../assets/images/coworkingsuites.jpg")} style={{ height: 250 }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text style={[gStyles.t4, { color: "gold", fontWeight: "bold", backgroundColor: "rgba(0,0,0,.3)" }]}>
+          SUITES
+        </Text>
+
+        <Text style={{ backgroundColor: "rgba(0,0,0,.5)", color: "gold", fontWeight: "bold" }}>
+          PRIVATE,LOCKABLE, AND PERFECT FOR A SMALL TEAM
+        </Text>
+
+        <Button
+          title={"AVAILABILTY"}
+          containerStyle={{ marginTop: 20 }}
+          titleStyle={{ color: "black" }}
+          buttonStyle={{ backgroundColor: "gold" }}
+          onPress={() => alert("Button Pressed")}
+        />
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default Row4;
--- views/HomeView.js
+++ views/HomeView.js
@@ -3,6 +3,7 @@ import { View, Text, Image, ScrollView, ImageBackground } from "react-native";
 import Row1 from "./Home/Row1";
 import Row2 from "./Home/Row2";
 import Row3 from "./Home/Row3";
+import Row4 from "./Home/Row4";
 
 export default function HomeView({ navigation }) {
   return (
@@ -11,9 +12,7 @@ export default function HomeView({ navigation }) {
         <Row1 />
         <Row2 />
         <Row3 />
-        <View style={{ height: 200, backgroundColor: "blue" }}>
-          <Text>Home View</Text>
-        </View>
+        <Row4 />
         <View style={{ height: 200, backgroundColor: "red" }}>
           <Text>Home View</Text>
         </View>
